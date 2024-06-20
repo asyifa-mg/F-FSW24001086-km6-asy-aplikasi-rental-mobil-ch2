344b3638c3faeddb68a93172c23307c14b687476
asyifa-mg <asyifamaharanigustina12@gmail.com>
2024-06-20 12:01:04 +0700

    update challenge

--- server/index.js
+++ server/index.js
@@ -1,22 +1,39 @@
+// core package=>built in node js atau bawaan node
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+
+// third Party Packages
 const url = require("url");
-const PUBLIC_DIRECTORY = path.join(__dirname, "../public");
+
+const PUBLIC_DIR = path.join(__dirname, "../public");
 const PORT = 8000;
 
 const server = (req, res) => {
   if (req.url === "/") {
     req.url = "/index.html";
-  } else if (req.url === "/car") {
+  } else if (req.url === "/cars") {
     req.url = "/cars.html";
   } else {
     req.url = req.url;
   }
 
+  const parseURL = url.parse(req.url);
+  const pathName = `${parseURL.pathname}`;
+  const extension = path.parse(pathName).ext;
+  const absolutePath = path.join(PUBLIC_DIR, pathName);
+
+  const contentTypes = {
+    ".css": "text/css",
+    ".png": "image/png",
+    ".svg": "image/svg+xml",
+    ".html": "text/html",
+    ".js": "text/javascript",
+  };
+
   fs.readFile(absolutePath, (err, data) => {
     if (err) {
-      res.statusCode = 500;
+      res.statusCode = 404;
       res.end("File not found ...");
     } else {
       res.setHeader("Content-Type", contentTypes[extension] || "text/plain");
